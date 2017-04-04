c0396be4d570265877ccc7cc87f0908a0ce39f77
zihua <1477765176@qq.com>
2017-04-04 09:46:48 +0800

    rabbitmq 的使用

---
+++ src/main/java/com/zihua/rabbitmq/Send.java
@@ -0,0 +1,29 @@
+package com.zihua.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Created by zihua on 17-4-4.
+ */
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World! ffffffffffffff";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+
+}
